0cf5cddb3765870189802d62a891573b81abbcb5
MinsuHan <trms7794han@gmail.com>
2023-09-08 11:30:38 +0900

    CI: github pages 배포 자동화 workflow 구성

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <GlobalStyle />
     <AuthContextProvider>
       <App />
