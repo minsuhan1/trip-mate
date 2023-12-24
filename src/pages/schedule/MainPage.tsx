839fb9cd39bf79e8c4ccf633db0596cbb037016b
minsuhan1 <trms7794han@gmail.com>
2023-12-24 17:37:26 +0900

    Fix: 최초 스케줄 로드 안되는 문제 픽스

--- src/pages/schedule/MainPage.tsx
+++ src/pages/schedule/MainPage.tsx
@@ -1,7 +1,40 @@
+import { useEffect } from "react";
 import Banner from "../../components/schedule/Banner";
 import ScheduleList from "../../components/schedule/ScheduleList";
+import { useParams } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../hooks/useApp";
+import { useLoadingState } from "../../contexts/loading-context";
+import { getScheduleList } from "../../store/scheduleReducer";
+import { useAuthState } from "../../contexts/auth-context";
 
 function MainPage() {
+  const tripId = useParams()?.tripId;
+  const isScheduleLoaded = useAppSelector(
+    (state) => state.scheduleListReducer
+  ).status;
+  const dispatch = useAppDispatch();
+  const { setLoading } = useLoadingState();
+  const authCtx = useAuthState();
+
+  useEffect(() => {
+    const loadSchedules = async () => {
+      if (authCtx.user) {
+        setLoading(true);
+        await dispatch(
+          getScheduleList({
+            uid: authCtx.user.uid,
+            tripId: tripId as string,
+          })
+        );
+        setLoading(false);
+      }
+    };
+
+    if (tripId && isScheduleLoaded !== "loaded") {
+      loadSchedules();
+    }
+  }, []);
+
   return (
     <>
       <Banner />
